a8194502c44b623629e9e90e4ca61d98c7c54387
khattak <jalwaus2000@googlemail.com>
2019-11-14 15:20:09 +0100

    Updates

---
+++ precache-manifest.fb25d1b18ee34e11089310abac93a8eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc6120d721daf781564c4c0e94d1db54",
+    "url": "/photo_gallery/index.html"
+  },
+  {
+    "revision": "35d46b63831334748d9e",
+    "url": "/photo_gallery/static/css/main.42b7c789.chunk.css"
+  },
+  {
+    "revision": "a313fe61df6145d4ee9f",
+    "url": "/photo_gallery/static/js/2.c1885eaa.chunk.js"
+  },
+  {
+    "revision": "35d46b63831334748d9e",
+    "url": "/photo_gallery/static/js/main.1de6c626.chunk.js"
+  },
+  {
+    "revision": "bc3f5c132b2f64c7504f",
+    "url": "/photo_gallery/static/js/runtime-main.2452996b.js"
+  },
+  {
+    "revision": "ad00812088b86bf34654980e11787687",
+    "url": "/photo_gallery/static/media/image.ad008120.jpg"
+  }
+]);
